f702260080a070f4e6ca8618846e55da3c268bd2
Mahir Muzemil <vybzmaha3@gmail.com>
2022-05-09 10:52:23 +0300

    virtual shooping android application devlopment using unity engine including arfoundation
    base for dev frontend.. focus on ui.... curentyl in progress
    contains bases for fileBrowser to get directory and file path.. will need other functionality
    ..upload to firebase storage
    ..GLTF runtime loader plugin..can load from local
    ..firebase authentication
    ..ui Interface..inprogress

---
+++ AR virtual/Assets/Scripts/FileBrowserUiComponent.cs
@@ -0,0 +1,101 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FileBrowserUiComponent : MonoBehaviour
+{
+    //each instance of fileBrowser ui component contains this data
+    //data is filled by FBmaneger
+    //responds to fbmaneger to navigate dir
+    //respons to fbmaneger with filePath
+    public Color fileIcon;
+    public Color dirIcon;
+    string displayName;
+    public bool isFile,isDir;
+    string filePath;
+    public void SeFiletData(string _displayName,string _filePath_){
+        isFile=true;
+        displayName=_displayName;
+        filePath=_filePath_;
+        updateComponent();
+        
+    }
+    public void SetDirData(string _displayName,string _path){
+        isDir=true;
+        displayName=_displayName;
+        filePath=_path;
+        updateComponent();
+        
+    }
+    void updateComponent(){
+        Text childDispayComp;
+       if(gameObject.GetComponentInChildren<Text>().gameObject.name=="displayName"){
+           childDispayComp=gameObject.GetComponentInChildren<Text>();
+       }else{
+           print("..component locating error");
+           return;
+       }
+       //continue
+       childDispayComp.text=displayName;
+       if(isFile){
+           childDispayComp.color=fileIcon;
+       }else if(isDir){
+           childDispayComp.color=dirIcon;
+       }
+       
+    }
+    public void onAction(){
+        if(isDir){
+            fileBrowserManeger.SwitchCurrentDir(filePath+displayName);
+            //update browser
+        }else if(isFile){
+            fileBrowserManeger.set_requestedFilePath(filePath+displayName);
+        }
+    }
+    public void setIcon(){
+        GameObject childDispayComp;
+        if(isDir&&!isFile){
+            if(gameObject.GetComponentInChildren<GameObject>().gameObject.name=="dirIcon"){
+                childDispayComp=gameObject.GetComponentInChildren<GameObject>().gameObject;
+                childDispayComp.SetActive(true);
+            }else if(gameObject.GetComponentInChildren<GameObject>().gameObject.name=="fileIcon"){
+                childDispayComp=gameObject.GetComponentInChildren<GameObject>().gameObject;
+                childDispayComp.SetActive(false);
+            }
+            else{
+                print("..component locating error");
+                return;
+            }
+            
+        }else if(isFile&&!isDir){
+            if(gameObject.GetComponentInChildren<Image>().gameObject.name=="dirIcon"){
+                childDispayComp=gameObject.GetComponentInChildren<Image>().gameObject;
+                childDispayComp.SetActive(false);
+            }else if(gameObject.GetComponentInChildren<Image>().gameObject.name=="fileIcon"){
+                childDispayComp=gameObject.GetComponentInChildren<Image>().gameObject;
+                childDispayComp.SetActive(true);
+            }
+            else{
+                
+                print("..component locating error");
+                return;
+            }
+        }else{
+            print("error..");
+        }
+      
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AR virtual/Assets/Scripts/modelLod.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Siccity.GLTFUtility;
+
+
+
+public class modelLod : MonoBehaviour
+{
+    public static string filepath;
+
+    
+    GameObject model;
+    // Start is called before the first frame update
+    void importModelFromFile()
+    {
+        
+        GameObject result = Importer.LoadFromFile(filepath);
+        model=result;
+        
+    }
+    public void displayModelRunT(){
+        importModelFromFile();
+        GameObject modeTransform=new GameObject();
+        Instantiate(model,modeTransform.transform);
+    }
+
+    // Update is called once per frame
+    public void chacking()
+    {
+       string tmp=Application.dataPath+"/files_test_resorces/cannon-gltf-embedded.gltf";
+       
+        filepath=tmp;
+        cloudManeger cman=this.gameObject.GetComponent<cloudManeger>();
+        cman.SetRefrence();
+        cman.uploadFileToCloud(filepath);//upload to cloud from filedir
+
+        string downloadedfilesDir=Application.dataPath+"/files_test_resorces";
+        cman.DownloadFromCloud("empty",downloadedfilesDir);
+        
+       // displayModelRunT();
+    }
+    public void getFilePath(){
+        //file explorer
+         string tmp=Application.dataPath;
+        filepath=tmp;
+        DirectoryInfo df=new DirectoryInfo(filepath);
+        FileInfo[] info=df.GetFiles();
+        foreach(FileInfo file in df.GetFiles()){
+            print(info.Length);
+            print(file.Name);
+        }
+    }
+}
