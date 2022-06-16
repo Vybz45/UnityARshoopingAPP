5b77521264b755c967974b85e80bafe23c80823d
Mahir Muzemil <vybzmaha3@gmail.com>
2022-06-16 23:09:57 +0300

    selling add product proccesing befor databease....

--- ARvirtual/Assets/SellingAddproduct.cs
+++ ARvirtual/Assets/SellingAddproduct.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections;
+﻿//verify and pass product data... for database and cloud
+//verify---> uploadToCloud--->getHaseValue--->packData--->sendToDatabase
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SellingAddproduct : MonoBehaviour
 {
+    public InputField ui_product_Title;
+    //product catagoris class needed
+    public InputField ui_product_discription;
+    public InputField ui_price_discription;
     public Text tem_outinfo_description;
     static string image_path_upload;
     public Text image_path_state;
+    public string cloud_image_hashValue;
+    public string cloud_model_hashValue;
     public Text model_path_state;
     static string model_path_upload;
+    public bool productData_valid;
     
     // Start is called before the first frame update
     void Start()
@@ -42,8 +51,31 @@ public class SellingAddproduct : MonoBehaviour
             model_path_state.color=Color.green;
             model_path_state.text="VALID";
         }else{
+            //model should be optional
             model_path_state.color=Color.red;
             model_path_state.text="REQUIRED";
         }
+    }
+    bool validateProductData(){
+        bool data_IsValid=false;
+        //chack data integristy...if null display massege box
+        //if succsefful display upload progress
+        //if complet change panel to selling... new added product are listed by(selling class)
+        return data_IsValid;
+    }
+    void Display_progress(bool _isProductDat_valid){
+        //call upload processes
+        //display upload progress
+        //get product hash value from cloudManeger
+        
+    }
+    void Database_addProduct(){
+
+        //set ui panale to selling
+    }
+    public void AddProductToDataBase(){
+        
+        Display_progress(validateProductData());
+        Database_addProduct();
     }
 }
